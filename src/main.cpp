d65b9e3093fa96b451aeae193f74e67eefa96f98
xnum <s000032001@gmail.com>
2016-10-25 16:04:37 +0800

    serve forever

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,8 @@
 
 ProcessController procCtrl;
 
-void buildConnection()
+int buildConnection()
 {
-
-
     int sockfd;
     int port = 4577;
 
@@ -48,121 +46,133 @@ void buildConnection()
         perror("listen() error");
     }
 
+    return sockfd;
+}
+
+void serveForever(int sockfd)
+{
     struct sockaddr_in cAddr;
     socklen_t len = sizeof(cAddr);
     bzero((char*)&cAddr, sizeof(cAddr));
 
-    int fdm = posix_openpt(O_RDWR);
-    if(fdm < 0) {
-        printf("posix_openpt() Error:%s\n",strerror(errno));
-        exit(1);
-    }
-
-    if(0 != grantpt(fdm)) {
-        printf("grantpt() Error:%s\n",strerror(errno));
-        exit(1);
-    }
-
-    if(0 != unlockpt(fdm)) {
-        printf("unlockpt() Error:%s\n",strerror(errno));
-        exit(1);
-    }
-
-    int fds = open(ptsname(fdm), O_RDWR);
     int rc = 0;
 
-    if(fork()) { //parent
+    while(1) {
 
+    WAIT_CONN:
         int connfd;
         if(0 > (connfd = accept(sockfd, (struct sockaddr*)&cAddr, &len))) {
             perror("accept() error");
         }
 
-        close(fds);
+        int fdm = posix_openpt(O_RDWR);
+        if(fdm < 0) {
+            printf("posix_openpt() Error:%s\n",strerror(errno));
+            exit(1);
+        }
 
-        while(1) {
-            char buff[256] = {};
+        if(0 != grantpt(fdm)) {
+            printf("grantpt() Error:%s\n",strerror(errno));
+            exit(1);
+        }
 
-            struct pollfd fds[2];
-            fds[0].fd = fdm;
-            fds[1].fd = connfd;
-            fds[0].events = POLLIN;
-            fds[1].events = POLLIN;
+        if(0 != unlockpt(fdm)) {
+            printf("unlockpt() Error:%s\n",strerror(errno));
+            exit(1);
+        }
 
-            rc = poll(fds, 2, -1);
+        int fds = open(ptsname(fdm), O_RDWR);
 
-            if(rc == -1) {
-                printf("select() Error:%s\n",strerror(errno));
-                exit(1);
-            }
-            else if(rc > 0) {
-                if(fds[0].revents & POLLIN) {
-                    rc = read(fds[0].fd, buff, sizeof(buff));
-                    if(rc > 0)
-                        write(fds[1].fd, buff, rc);
-                    else if(rc < 0) {
-                        printf("read() connfd Error:%s\n",strerror(errno));
-                        exit(1);
-                    }
+        if(fork()) { //parent
+
+            close(fds);
+
+            while(1) {
+                char buff[256] = {};
+
+                struct pollfd fds[2];
+                fds[0].fd = fdm;
+                fds[1].fd = connfd;
+                fds[0].events = POLLIN;
+                fds[1].events = POLLIN;
+
+                rc = poll(fds, 2, -1);
+
+                if(rc == -1) {
+                    printf("select() Error:%s\n",strerror(errno));
+                    exit(1);
                 }
-
-                if(fds[1].revents & POLLIN) {
-                    rc = read(fds[1].fd, buff, sizeof(buff));
-                    if(rc > 0)
-                        write(fds[0].fd, buff, rc);
-                    else if(rc < 0) {
-                        printf("read() connfd Error:%s\n",strerror(errno));
-                        exit(1);
+                else if(rc > 0) {
+                    if(fds[0].revents & POLLIN) {
+                        rc = read(fds[0].fd, buff, sizeof(buff));
+                        if(rc > 0)
+                            write(fds[1].fd, buff, rc);
+                        else if(rc < 0) {
+                            printf("read() connfd Error:%s\n",strerror(errno));
+                            exit(1);
+                        }
                     }
-                }
 
-                for(int i = 0 ; i < 2 ; ++i) {
-                    if(fds[i].revents & POLLHUP || fds[i].revents & POLLERR) {
-                        close(fds[0].fd);
-                        close(fds[1].fd);
-                        exit(0);
+                    if(fds[1].revents & POLLIN) {
+                        rc = read(fds[1].fd, buff, sizeof(buff));
+                        if(rc > 0)
+                            write(fds[0].fd, buff, rc);
+                        else if(rc < 0) {
+                            printf("read() connfd Error:%s\n",strerror(errno));
+                            exit(1);
+                        }
+                    }
+
+                    /* connfd or fdm exited */
+                    for(int i = 0 ; i < 2 ; ++i) {
+                        if(fds[i].revents & POLLHUP || fds[i].revents & POLLERR) {
+                            fprintf(stderr,"connect over");
+                            close(fds[0].fd);
+                            close(fds[1].fd);
+                            goto WAIT_CONN;
+                        }
                     }
                 }
             }
         }
-        exit(1);
-    }
-    else { // child
-        struct termios slave_orig_term_settings; // Saved terminal settings
-        struct termios new_term_settings; // Current terminal settings
+        else { // child
+            struct termios slave_orig_term_settings; // Saved terminal settings
+            struct termios new_term_settings; // Current terminal settings
 
-        // CHILD
+            // CHILD
 
-        // Close the master side of the PTY
-        close(fdm);
-        close(sockfd);
+            // Close the master side of the PTY
+            close(fdm);
+            close(sockfd);
 
-        // Save the defaults parameters of the slave side of the PTY
-        rc = tcgetattr(fds, &slave_orig_term_settings);
+            // Save the defaults parameters of the slave side of the PTY
+            rc = tcgetattr(fds, &slave_orig_term_settings);
 
-        // Set RAW mode on slave side of PTY
-        new_term_settings = slave_orig_term_settings;
-        cfmakeraw (&new_term_settings);
-        tcsetattr (fds, TCSANOW, &new_term_settings);
+            // Set RAW mode on slave side of PTY
+            new_term_settings = slave_orig_term_settings;
+            cfmakeraw (&new_term_settings);
+            tcsetattr (fds, TCSANOW, &new_term_settings);
 
-        // The slave side of the PTY becomes the standard input and outputs of the child process
-        close(0); // Close standard input (current terminal)
-        close(1); // Close standard output (current terminal)
-        close(2); // Close standard error (current terminal)
+            // The slave side of the PTY becomes the standard input and outputs of the child process
+            close(0); // Close standard input (current terminal)
+            close(1); // Close standard output (current terminal)
+            close(2); // Close standard error (current terminal)
 
-        dup(fds); // PTY becomes standard input (0)
-        dup(fds); // PTY becomes standard output (1)
-        dup(fds); // PTY becomes standard error (2)
+            dup(fds); // PTY becomes standard input (0)
+            dup(fds); // PTY becomes standard output (1)
+            dup(fds); // PTY becomes standard error (2)
 
-        // Now the original file descriptor is useless
-        close(fds);
+            // Now the original file descriptor is useless
+            close(fds);
 
-        // Make the current process a new session leader
-        setsid();
+            // Make the current process a new session leader
+            setsid();
 
-        // As the child is a session leader, set the controlling terminal to be the slave side of the PTY
-        // (Mandatory for programs like the shell to make them manage correctly their outputs)
-        ioctl(0, TIOCSCTTY, 1);
+            // As the child is a session leader, set the controlling terminal to be the slave side of the PTY
+            // (Mandatory for programs like the shell to make them manage correctly their outputs)
+            ioctl(0, TIOCSCTTY, 1);
+            return;
+        }
     }
 }
 
@@ -200,7 +210,8 @@ void backToShell(int sig __attribute__((unused))) {
 
 int main()
 {
-    buildConnection();
+    int sockfd = buildConnection();
+    serveForever(sockfd);
 
     procCtrl.SetShellPgid(getpgid(getpid()));
     procCtrl.SetupPwd();
